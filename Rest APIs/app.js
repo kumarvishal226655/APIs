a244838c02f924113c251e3e18d32afd04a48475
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-09-08 21:37:30 +0530

    paths

--- Rest APIs/app.js
+++ Rest APIs/app.js
@@ -33,6 +33,10 @@ app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
 });
 
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });

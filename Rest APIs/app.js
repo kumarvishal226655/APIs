0c0a9c8f2e46327cfb7df1c98ce5c2a3de5ecdb1
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-09-08 21:03:00 +0530

    create array for posts

--- Rest APIs/app.js
+++ Rest APIs/app.js
@@ -1,21 +1,38 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 3000;
-const path = require('path');
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, 'public')));
+app.set(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+let posts = [
+  {
+    username: "john_doe",
+    title: "First Post",
+    content: "This is my first post!",
+  },
+  {
+    username: "jane_smith",
+    title: "Hello World",
+    content: "Hello everyone, this is Jane.",
+  },
+  {
+    username: "alice_wonder",
+    title: "Adventures",
+    content: "I love adventures and exploring new places.",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
